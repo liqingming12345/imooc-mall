868dcfaa2e0725391174e20471473f328751c127
apple-lab <apple>
2021-09-11 11:39:36 +0800

    init

---
+++ src/main/java/com/imooc/mall/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.imooc.mall.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.mall.common.ApiRestResponse;
+import com.imooc.mall.model.request.CreateOrderReq;
+import com.imooc.mall.model.vo.OrderVO;
+import com.imooc.mall.service.OrderService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 描述 ： 订单Controller
+ */
+@RestController
+public class OrderController {
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("order/create")
+    @ApiOperation("创建订单")
+    public ApiRestResponse create(@RequestBody CreateOrderReq createOrderReq){
+        String orderNo = orderService.create(createOrderReq);
+        return ApiRestResponse.success(orderNo);
+    }
+
+    @GetMapping("order/detail")
+    @ApiOperation("前台订单详情")
+    public ApiRestResponse detail(@RequestParam String orderNo) {
+        OrderVO orderVO = orderService.detail(orderNo);
+        return ApiRestResponse.success(orderVO);
+    }
+
+    @GetMapping("order/list")
+    @ApiOperation("前台订单列表")
+    public ApiRestResponse list(@RequestParam Integer pageNum,
+       @RequestParam Integer pageSize) {
+        PageInfo pageInfo = orderService.listForCustomer(pageNum, pageSize);
+        return ApiRestResponse.success(pageInfo);
+    }
+}
---
+++ src/main/java/com/imooc/mall/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.imooc.mall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.mall.model.request.CreateOrderReq;
+import com.imooc.mall.model.vo.OrderVO;
+
+/**
+ * 描述：     订单Service
+ */
+public interface OrderService {
+
+
+    String create(CreateOrderReq createOrderReq);
+
+    OrderVO detail(String orderNo);
+
+    PageInfo listForCustomer(Integer pageNum, Integer pageSize);
+}
